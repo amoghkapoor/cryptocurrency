21c1f1992a71576446dc20ef3f60b6310015a599
Amogh Kapoor <chatur.chaturx@gmail.com>
2023-10-17 23:23:46 +0530

    Update API changes

--- src/App.js
+++ src/App.js
@@ -45,7 +45,6 @@ const App = () => {
                     </Typography.Title>
                     <Space>
                         <Link to="/">Home</Link>
-                        <Link to="/exchanges">Exchanges</Link>
                         <Link to="/news">News</Link>
                     </Space>
                 </div>
--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -17,10 +17,10 @@ export const cryptoApi = createApi({
             query: (count) => createRequest(`/coins?limit=${count}`)
         }),
         getCryptoDetails: builder.query({
-            query: (coinId) => createRequest(`/coin/${coinId}`)
+            query: (coinId) => createRequest(`/coin/${coinId}`),
         }),
         getCryptoHistory: builder.query({
-            query: ({ coinId, timePeriod }) => createRequest(`/coin/${coinId}/history/${timePeriod}`)
+            query: ({ coinId, timePeriod }) => createRequest(`/coin/${coinId}/history/?timePeriod=${timePeriod}`)
         }),
         getExchanges: builder.query({
             query: () => createRequest('/exchanges'),
